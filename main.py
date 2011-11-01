8b5a7e29ab107ded7003be2d3ba496bc6d61b996
Thiago Avelino <thiagoavelinoster@gmail.com>
2011-11-01 14:12:02 -0200

    update

--- main.py
+++ main.py
@@ -15,10 +15,15 @@ class MainHandler(webapp.RequestHandler):
         try:
             _file = self.request.POST['arquivo'].file.read()
             data = json.loads(_file)
+
             i = 1
             for _o in data["friends"]:
                 link = _o["profileIds"][0]
-                self.response.out.write('%i - <a href="https://plus.google.com/u/0/%s" target="_black">https://plus.google.com/u/0/%s</a> <br />' % (i, link, link))
+                name = _o["displayName"]
+                self.response.out.write('%i - <a \
+                        href="https://plus.google.com/u/0/%s" \
+                        target="_black">https://plus.google.com/u/0/%s \
+                        </a> %s <br />' % (i, link, link, name))
                 i = i +1
         except:
             if i <= 1:
